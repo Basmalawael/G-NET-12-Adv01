05d9b803ad11836ba7295e8364fa2b5c6f47c7d9
Basmala Wael <basmalawael86@gmail.com>
2026-03-28 01:23:58 +0700

    Q3:

--- G-NET-12-Adv01/Program.cs
+++ G-NET-12-Adv01/Program.cs
@@ -61,7 +61,7 @@ namespace G_NET_12_Adv01
             #endregion
 
             #region Q3:
-
+             
             #region 1
             // What are multiple type parameters? 
             /*
